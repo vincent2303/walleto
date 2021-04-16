91299dfa5a0217fca7168deecea7a252b9a83c75
Vincent <vincentm@sipios.com>
2021-04-16 12:12:52 +0100

    move styles in Stylesheet

--- App.tsx
+++ App.tsx
@@ -2,28 +2,28 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ParamList } from './global/types';
+import { colors } from './global/styles';
 
 import WalletList from './screens/WalletList';
 import Overview from './screens/Overview';
 import NewWallet from './screens/NewWallet';
 import InsideWallet from './screens/InsideWallet';
-import { colors } from './global/styles';
 
 const Stack = createStackNavigator<ParamList>();
 const Tab = createBottomTabNavigator<ParamList>();
 
 const homeBottomTab = () => (
-  <Tab.Navigator tabBarOptions={{ style: { backgroundColor: colors.darkBlack } }}>
+  <Tab.Navigator tabBarOptions={{ style: styles.bottomTabBar }}>
     <Tab.Screen name="walletList" component={WalletList} />
     <Tab.Screen name="overview" component={Overview} />
   </Tab.Navigator>
 );
 
 const App = () => (
-  <SafeAreaView style={{ flex: 1, backgroundColor: colors.darkBlack }}>
+  <SafeAreaView style={styles.container}>
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ header: () => null }}>
         <Stack.Screen name="home" component={homeBottomTab} />
@@ -34,5 +34,14 @@ const App = () => (
     <StatusBar barStyle="light-content" />
   </SafeAreaView>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, backgroundColor: colors.darkBlack,
+  },
+  bottomTabBar: {
+    backgroundColor: colors.darkBlack,
+  },
+});
 
 export default App;
